ebe32ad4ac50e242fa6b9d2f5f79bb790c87953f
aiprotips <valenzosrls@gmail.com>
2025-09-09 13:19:00 +0200

    Update config.py

--- AI_PRO_TIPS/config.py
+++ AI_PRO_TIPS/config.py
@@ -28,17 +28,87 @@ class Config:
         ],
     }
 
-    # Quiet hours & scheduling
-    QUIET_HOURS = (0, 8)  # 00:00–08:59
+    # Quiet hours: 00:00–07:59
+    QUIET_HOURS = (0, 8)
     MIN_GAP_MINUTES = int(os.getenv("MIN_GAP_MINUTES", "18"))
     SLOTS = {
-        "morning":  ("09:15", "11:45"),
+        "morning":  ("08:00", "11:45"),
         "lunch":    ("12:00", "14:00"),
         "afternoon":("15:00", "18:30"),
-        "evening":  ("19:30", "23:15"),
+        "evening":  ("19:30", "23:59"),
     }
 
     LIVE_POLL_SECONDS = int(os.getenv("LIVE_POLL_SECONDS", "75"))
     AUTOPILOT_TICK_SECONDS = int(os.getenv("AUTOPILOT_TICK_SECONDS", "300"))
 
-    MONITOR_LEAGUES = None  # e.g., [39,140,135,78,61]
+    # Leghe consentite
+    ALLOWED_LEAGUES = [
+        # Italia
+        135,   # Serie A
+        136,   # Serie B
+        137,   # Serie C
+        318,   # Coppa Italia
+
+        # Inghilterra
+        39,    # Premier League
+        40,    # Championship
+        45,    # FA Cup
+        46,    # EFL Cup
+
+        # Spagna
+        140,   # La Liga
+        141,   # La Liga 2
+        143,   # Copa del Rey
+
+        # Germania
+        78,    # Bundesliga
+        79,    # 2. Bundesliga
+        81,    # DFB Pokal
+
+        # Francia
+        61,    # Ligue 1
+        62,    # Ligue 2
+        66,    # Coupe de France
+
+        # Portogallo
+        94,    # Primeira Liga
+        96,    # Taça de Portugal
+
+        # Olanda
+        88,    # Eredivisie
+        90,    # KNVB Beker
+
+        # Belgio
+        144,   # Jupiler Pro League
+
+        # Austria
+        218,   # Bundesliga
+
+        # Danimarca
+        103,   # Superligaen
+
+        # Romania
+        283,   # Liga I
+
+        # Scozia
+        179,   # Premiership
+
+        # Svizzera
+        207,   # Super League
+
+        # Turchia
+        203,   # Super Lig
+
+        # Competizioni internazionali
+        2,     # UEFA Champions League
+        3,     # UEFA Europa League
+        848,   # Conference League
+        4,     # World Cup
+        10,    # Copa Libertadores
+        11,    # Copa Sudamericana
+
+        # Extra consigliati
+        71,    # Brasile Serie A
+        128,   # Argentina Primera Division
+        253,   # MLS (USA)
+    ]
